e70603d52ce3ea6d6947c60393a40e8908ceff7c
Jean-François Roche <jfroche@affinitic.be>
2013-03-08 17:15:08 +0100

    add logging for query

--- src/affinitic/db/event.py
+++ src/affinitic/db/event.py
@@ -7,9 +7,13 @@ Copyright by Affinitic sprl
 
 $Id$
 """
+import time
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 from zope.interface import implements
-from affinitic.db.interfaces import IPGDBInitialized, IOracleDBInitialized, \
-                                    IMSSqlDBInitialized
+from affinitic.db.log import log_long_query
+from affinitic.db.interfaces import (IPGDBInitialized, IOracleDBInitialized,
+                                     IMSSqlDBInitialized)
 
 
 class PGDBInitialized(object):
@@ -31,3 +35,16 @@ class MSSqlDBInitialized(object):
 
     def __init__(self, objectToAdapt):
         self.object = objectToAdapt
+
+
+def register_logging():
+    @event.listens_for(Engine, "before_cursor_execute")
+    def before_cursor_execute(conn, cursor, statement, parameters, context,
+                              executemany):
+        context._query_start_time = time.time()
+
+    @event.listens_for(Engine, "after_cursor_execute")
+    def after_cursor_execute(conn, cursor, statement, parameters, context,
+                             executemany):
+        total = time.time() - context._query_start_time
+        log_long_query(conn.engine, total, statement, parameters)
---
+++ src/affinitic/db/log.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import logging
+import inspect
+import os
+
+
+def need_to_log(total_time):
+    log_min_time = os.environ.get('SA_MIN_LOG_SLOW_SQL', 3)
+    if total_time >= float(log_min_time):
+        return True
+    return False
+
+
+def get_arsia_frame():
+    ignored_packages = ['SQLAlchemy', 'affinitic.db', 'dogpile.cache']
+    for frame, file_path, linenumber, fname, line, i in inspect.stack()[1:]:
+        if [ignored for ignored in ignored_packages if ignored in file_path]:
+                continue
+        return frame
+
+
+def log_long_query(engine, total_time, statement, parameters):
+    if not need_to_log(total_time):
+        return
+    frame = get_arsia_frame()
+    if frame is not None:
+        function = "%s()" % frame.f_code.co_name
+        file_line = "%s:%s" % (frame.f_code.co_filename, frame.f_lineno)
+    else:
+        function = file_line = 'UNKNOWN'
+    instance_name = os.path.split(os.environ.get('INSTANCE_HOME', '.'))[-1]
+    dbdriver = "%s@%s" % (engine.url.drivername, engine.url.host)
+    logger = logging.getLogger('affinitic.db')
+    logger.warning("Long query: %ss \n %s \n params: %s \n db: %s \n instance: %s \n function: %s \n file/line: %s" % (total_time, statement, repr(parameters), dbdriver, instance_name, function, file_line))
